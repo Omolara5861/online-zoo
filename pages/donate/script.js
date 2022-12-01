c9012fde9469888bbd6178ac5e41864ec094a8bc
Omolara5861 <debbiegterra@gmail.com>
2022-12-01 10:06:30 +0100

    refactor: :recycle: Make input for amount 1000 checked on large screens
    Use Javascript to dynamically check input for amount 1000 when the screen width hits large screens size
    
    Before the change, when the page loads 1000 is checked by default, when it is set to tablet screen size 100 becomes checked instead but when it is set back to a large screen size, no input is checked. This change fixed that bug

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -1,4 +1,5 @@
 const amountChecked = document.getElementById('100');
+const defaultAmountChecked = document.getElementById('1000');
 
 const smallDevice = window.matchMedia("(max-width: 900px)");
 
@@ -6,6 +7,7 @@ smallDevice.addListener(handleDeviceChange);
 
 function handleDeviceChange(e) {
   if (e.matches) amountChecked.checked = true;
+  else defaultAmountChecked.checked = true;
 }
 
 // Run it initially

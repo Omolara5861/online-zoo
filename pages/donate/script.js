6755e40fb15f5d5a36da533c7e0364390845d899
Omolara5861 <debbiegterra@gmail.com>
2022-12-03 11:23:25 +0100

    feat: :necktie: Update checked amount to another amount input value
    If the entered value in another amount matches any of the amount on the donation range bar, that amount would be checked

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -1,7 +1,9 @@
-const donateForm = document.getElementById("donate-form");
+const donateForm = document.getElementById('donate-form');
+const anotherAmount = document.getElementById('another-amount');
 const donatedAmount = donateForm.money;
 
 donateForm.anotherAmount.value = 100;
+
 
 donateForm.addEventListener('change', updateDonateAmount);
 
@@ -10,3 +12,13 @@ function updateDonateAmount() {
         if (amount.checked === true) donateForm.anotherAmount.value = amount.id;
     }
 }
+
+
+anotherAmount.addEventListener('input', updateCheckedAmount);
+
+function updateCheckedAmount() {
+    for (let amount of donatedAmount) {
+        if (anotherAmount.value === amount.id) amount.checked = true;
+    }
+}
+updateCheckedAmount();

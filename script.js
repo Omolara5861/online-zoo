52096cb9f4d86fa855aadf58bb208308682c6927
Omolara5861 <debbiegterra@gmail.com>
2022-12-03 14:18:19 +0100

    feat: :sparkles: Create pop-up overlay

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
+const overlay = document.getElementById("popup");
 
 menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
+    overlay.classList.toggle('active');
 });
 
+overlay.addEventListener('click', () => {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+    overlay.classList.toggle('active')
+});
